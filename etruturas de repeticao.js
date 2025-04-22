c0fbd0e86f05c5750d9d47f71093ccc30c5e3a5d
GITjoelinton <joelintoSiqueira@gmail.com>
2025-04-22 20:42:23 -0300

    atividade

---
+++ etruturas de repeticao.js
@@ -0,0 +1,10 @@
+console.log("contagem regressiva de 10 a 0");
+for (let i = 10; i >= 0; i--) {
+    console.log(i);
+}
+console.log("tempo esgotado!");
+
+
+for (let i = 1; i <= 10; i++) {
+    console.log("5 x " + i + " = " + 5 * i);
+}
