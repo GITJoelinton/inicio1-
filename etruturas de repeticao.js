e199f0cd44e904c09aa144f88b34c38be9536509
GITjoelinton <joelintoSiqueira@gmail.com>
2025-04-22 21:10:27 -0300

    aividade

--- etruturas de repeticao.js
+++ etruturas de repeticao.js
@@ -1,10 +1,12 @@
+//contagem regressiva//
 console.log("contagem regressiva de 10 a 0");
 for (let i = 10; i >= 0; i--) {
     console.log(i);
 }
 console.log("tempo esgotado!");
 
+//tabuada do 5//
 
-for (let i = 1; i <= 10; i++) {
+for (let i = 0; i <= 10; i++) {
     console.log("5 x " + i + " = " + 5 * i);
 }
