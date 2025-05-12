12435c4b1529c211283f0ffa4c74eade841000b9
GITjoelinton <joelintoSiqueira@gmail.com>
2025-05-12 19:29:27 -0300

    primeira aplicaçao

---
+++ orientacao objetos classes/uso de classe.js
@@ -0,0 +1,12 @@
+class notas {
+    constructor(nome,nota1,nota2) {
+        this.nome = nome;
+        this.nota1 =nota1;
+        this.nota2= nota2;
+    }
+    constnota1=new nota1(matematica,5);
+    constnota2=new this.nota2(biologia,6);
+    calcularValorTotal(){
+        return this.nota1*nota2;
+    }  
+}
--- sql.sql
+++ sql.sql
@@ -1,7 +1,1 @@
-USE joelinton;
-CREATE TABLE produtos(
-id INT PRIMARY KEY AUTO_INCREMENT,
-nome VARCHAR(255),
-preco DECIMAL(10,2)
-);usersmeuprojetomeuprojeto
-
+ALTER TABLE produto_id INT AUTO_INCREMENT;
